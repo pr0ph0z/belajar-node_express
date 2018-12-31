bb5fc100b310880a77e54c7808ab9858707f7d05
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-12-31 21:10:11 +0700

    idk what happends if we don't use bp. post berez

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const helper = require('./helper')
 
 const app = express()
 const port = 8080
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 let obj = [
     {
@@ -33,6 +37,10 @@ app.get('/users', function(req,res) {
 app.get('/user/:id', function(req, res) {
     find = helper.findData(obj, req.params.id)
     res.send((find == undefined ? "No data found!" : find))
+})
+
+app.post('/user', function(req, res) {
+    res.send(Object.keys(req.body).length != 0 ? req.body : "No data sent!")
 })
 
 app.listen(port, function() {
