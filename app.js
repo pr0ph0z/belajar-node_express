00bfcc083fbbae16e70bc420a046698932dd909e
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-12-31 17:10:11 +0700

    Coba dinamis port

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 
 const app = express()
+const port = 8080
 
 app.get('/', function(req, res) {
     res.send('Lg ap?')
 })
 
-app.listen('3000')
+app.listen(port, function() {
+    console.log("App listening on port %d", port)
+})
